7c27369d4f63d6dcff34e0a39c54cc9a189ac99e
FasdSnake <fasd.snake@gmail.com>
2014-06-16 02:48:39 +0000

    -
    temp commit

--- main.c
+++ main.c
@@ -20,11 +20,11 @@ void read_prog( FILE *file )
 void reg_debug( dcpu16_t *dcpu )
 {
     /* data registers */
-    printf( " A=0x%04x  B=0x%04x  C=0x%04x  X=0x%04x  Y=0x%04x  Z=0x%04x  I=0x%04x  J=0x%04x\n",
+    printf( " A=0x%04x  B=0x%04x  C=0x%04x \n X=0x%04x  Y=0x%04x  Z=0x%04x \n I=0x%04x  J=0x%04x\n",
             dcpu->A, dcpu->B, dcpu->C, dcpu->X, dcpu->Y, dcpu->Z, dcpu->I, dcpu->J);
 
     /* state/extra */
-    printf( "PC=0x%04x SP=0x%04x EX=0x%04x IA=0x%04x IAQ=0x%04x clocks=%i\tstate=%s\n",
+    printf( "PC=0x%04x SP=0x%04x EX=0x%04x IA=0x%04x \nIAQ=0x%04x clocks=%i\tstate=%s\n",
             dcpu->PC, dcpu->SP, dcpu->EX, dcpu->IA, dcpu->IAQ, dcpu->clocks, state_strs[dcpu->state]);
 
     printf( "\n" );
@@ -60,18 +60,9 @@ int main( int argc, char ** argv )
     struct timeval start, end;
     while( !dcpu_complete( &dcpu ) && !killed )
     {
-        gettimeofday( &start, NULL );
         int ticks = dcpu_tick( &dcpu );
-
-        gettimeofday( &end, NULL );
-        int elapsed = (end.tv_sec - start.tv_sec)*1000*1000 + (end.tv_usec - start.tv_usec);
-
-        int to_sleep = 10*ticks - elapsed;
-
-        if( to_sleep > 0 )
-        {
-            usleep( to_sleep );
-        }
+        reg_debug ( &dcpu );
+        sleep (1);
     }
 
     struct pollfd pollinfo[1];

e15098c3ef059353f36415e7d88ccd87207de703
FasdSnake <fasd.snake@gmail.com>
2014-06-18 15:54:21 +0000

    add func A=1 to Fasd Printer

--- hardware/printer.c
+++ hardware/printer.c
@@ -4,8 +4,7 @@
 #define HW_VER      0x0001
 #define AUTHOR_ID   0xFA5D0000
 
-/* interrupt types */
-#define PRINTER_PRINT 0x0000
+uint8_t buffer[0x10000];
 
 /* Initialize */
 void *init_printer (void)
@@ -26,13 +25,30 @@ void interrupt_printer (dcpu16_t *dcpu, dcpu_hardware_t *hardware)
     uint32_t c = dcpu->C;
     switch (a)
     {
-        case PRINTER_PRINT:
+        case 0x0000:
             if ( b+c > 0x10000 )
             {
                 dcpu->A = 0xFFFF;
                 return;
             }
             dcpu->A = fwrite (dcpu->memory + b, 2*c, 1, stdout);
+            fflush (stdout);
+            return;
+        case 0x0001:
+            if ( b+c > 0x10000 )
+            {
+                dcpu->A = 0xFFFF;
+                return;
+            }
+            uint16_t *p, *end;
+            p   = dcpu->memory + b;
+            end = dcpu->memory + b+c;
+            int i;
+            for (i=0 ; p<end; p++, i++)
+            {
+                buffer[i] = (*p) & 0x00FF;
+            }
+            dcpu->A = fwrite (buffer, c, 1, stdout);
             fflush (stdout);
             return;
     }
